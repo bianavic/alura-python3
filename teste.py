a57d7c259d1f447d10a1d961cfe34e32b9cdad0b
bianavic <nianavic@gmail.com>
2019-08-14 19:07:01 -0300

    exercicios aulas 01, 02, 03

---
+++ teste.py
@@ -0,0 +1,52 @@
+""" minha_idade = 26
+idade_namorado = 25
+
+if(minha_idade == idade_namorado):
+    print("temos idades iguais")
+else:
+    print("temos idades diferentes") """
+
+""" numero1 = 10
+numero2 = 10
+if(numero1 == numero2):
+    print("São números iguais") """
+
+""" idade1 = 10
+idade2 = "20"
+print(idade1 + idade2) """
+
+""" nome = "Nico"
+sobrenome = "Steppat"
+print(nome + sobrenome) """
+
+""" idade_str = input("Digite sua idade: ")
+idade = int(idade_str)
+
+if (idade > 18):
+    print("Você é maior de idade.")
+else:
+    if (idade < 12):
+        print("Você é uma criança.")
+    elif (idade > 12):
+        print("Você é um adolescente.") """
+
+
+""" idade_str = input("Digite sua idade: ")
+idade = int(idade_str)
+
+maior_idade = idade > 18
+crianca     = idade < 12
+adolescente = idade > 12 """
+
+usuario = input("Informe o usuário do sistema!: ")
+
+if(usuario == "Flavio"):
+    print("Seja bem-vindo Flávio!")
+else:
+    if(usuario == "Douglas"):
+        print("Seja bem-vindo Douglas")
+    else:
+        if(usuario == "Nico"):
+            print("Seja bem-vindo Nico")
+        else:
+            print("Usuário não identificado!")
