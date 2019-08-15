676ca6dca8848a8a76671bd1764a0bf0571f7fc4
bianavic <nianavic@gmail.com>
2019-08-14 21:32:35 -0300

    usando while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,15 +3,16 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 37
-chute_str = input("Digite o seu numero: ")
+total_de_tentativas = 3
 
-print("Voce digitou ", chute_str)
+while (total_de_tentativas >= 0):
+    chute_str = input("Digite o seu numero: ")
+    print("Voce digitou ", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
-
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
 if(acertou):
     print("Você acertou!")
